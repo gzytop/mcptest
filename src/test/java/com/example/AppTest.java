5dd66440c38189e57f1967e5e3cf6b2fcdcfab3d
gzytop <147611805+gzytop@users.noreply.github.com>
2025-05-30 16:31:39 +0800

    添加测试类

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * 单元测试示例
+ */
+public class AppTest {
+    /**
+     * 简单测试
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
